02b6e4d315f17457d2b42164e2f023cb619711fe
Sergei G <sergeig.public@gmail.com>
2016-09-23 23:46:07 -0700

    fixed lint messages

--- maint.go
+++ maint.go
@@ -7,64 +7,63 @@ import (
 	"log"
 	"os"
 
+	// SQLite driver is required
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// Executes upgrade procedure on SQLite db_file database file
+// UpgradeSQLite executes upgrade procedure on SQLite db_file database file
 // given sql_dir directory with SQLite upgrade scripts.
-func UpgradeSQLite(db_file, sql_dir string) (version int, err error) {
-	log.Printf("DB file: %v, SQL files directory: %v\n", db_file, sql_dir)
+func UpgradeSQLite(dbFile, sqlDir string) (version int, err error) {
+	log.Printf("DB file: %v, SQL files directory: %v\n", dbFile, sqlDir)
 
-	db, err := sql.Open("sqlite3", db_file)
+	db, err := sql.Open("sqlite3", dbFile)
 	if err != nil {
 		return -1, err
 	}
 	defer db.Close()
 
 	query := "pragma user_version"
-	var user_version int
-	err = db.QueryRow(query).Scan(&user_version)
+	var userVersion int
+	err = db.QueryRow(query).Scan(&userVersion)
 	if err != nil {
 		return -1, fmt.Errorf("%q: %s", err, query)
-	} else {
-		log.Printf("Loaded version: %v\n", user_version)
 	}
+	log.Printf("Loaded version: %v\n", userVersion)
 
-	user_version++
-	update_sql_file := fmt.Sprintf("%v/%04d.sql", sql_dir, user_version)
-	_, err = os.Stat(update_sql_file)
-	log.Printf("Checking if version file %v exists.\n", update_sql_file)
+	userVersion++
+	fileName := fmt.Sprintf("%v/%04d.sql", sqlDir, userVersion)
+	_, err = os.Stat(fileName)
+	log.Printf("Checking if version file %v exists.\n", fileName)
 	// loop while file exists
 	for !os.IsNotExist(err) {
 
 		// SQL upgrade file exists
 
-		log.Printf("Processing %v file...\n", update_sql_file)
+		log.Printf("Processing %v file...\n", fileName)
 		var content []byte
-		content, err = ioutil.ReadFile(update_sql_file)
+		content, err = ioutil.ReadFile(fileName)
 		if err != nil {
-			return user_version - 1, fmt.Errorf("Failed to read %v file content. Error: %v", update_sql_file, err)
+			return userVersion - 1, fmt.Errorf("Failed to read %v file content. Error: %v", fileName, err)
 		}
 		query = string(content)
 
 		_, err = db.Exec(query)
 		if err != nil {
-			return user_version - 1, fmt.Errorf("Failed to execute %v file content. Error: %v", update_sql_file, err)
+			return userVersion - 1, fmt.Errorf("Failed to execute %v file content. Error: %v", fileName, err)
 		}
 
 		// pragma values can not be parametrized (in python), so we use sring format and enforce integer value
-		query = fmt.Sprintf("pragma user_version = %v", user_version)
+		query = fmt.Sprintf("pragma user_version = %v", userVersion)
 		_, err = db.Exec(query)
 		if err != nil {
-			return user_version - 1, fmt.Errorf("Failed to update DB version to %v after %v file was executed. Error: %v\n",
-				user_version, update_sql_file, err)
-		} else {
-			log.Printf("Updated to version %v.\n", user_version)
+			return userVersion - 1, fmt.Errorf("Failed to update DB version to %v after %v file was executed. Error: %v\n",
+				userVersion, fileName, err)
 		}
+		log.Printf("Updated to version %v.\n", userVersion)
 
-		user_version++
-		update_sql_file = fmt.Sprintf("%v/%04d.sql", sql_dir, user_version)
-		_, err = os.Stat(update_sql_file)
+		userVersion++
+		fileName = fmt.Sprintf("%v/%04d.sql", sqlDir, userVersion)
+		_, err = os.Stat(fileName)
 	}
-	return user_version - 1, nil
+	return userVersion - 1, nil
 }
